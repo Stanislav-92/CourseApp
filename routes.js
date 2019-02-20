f8f5572938123e991850b04d390b5e43072a3f1f
Stanislav Chepa <schepa@softserveinc.com>
2019-02-20 18:30:35 +0200

    heroku commit

--- routes.js
+++ routes.js
@@ -10,9 +10,9 @@ let line1;
 let line2;
 let db;
 
-MongoClient.connect("mongodb://localhost:27017",  {useNewUrlParser: true, poolSize: 10})
+MongoClient.connect("mongodb://heroku_8z95sck1:702f73gt572chdjmo09ttrh4bs@ds237955.mlab.com:37955/heroku_8z95sck1",  {useNewUrlParser: true, poolSize: 10})
   .then(client => {
-    db = client.db('chartDatabase');
+    db = client.db('heroku_8z95sck1');
     chartsCollection = db.collection('charts');
     chartsCollection.findOne({"chartID.value": "col1"}).then(response => col1 = response).catch(error => console.error(error));
     chartsCollection.findOne({"chartID.value": "col2"}).then(response => col2 = response).catch(error => console.error(error));
